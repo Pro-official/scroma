25645899f73891bea8948a3faf0df8079aaea5f4
Pro-official <promise.official000@gmail.com>
2025-09-18 04:35:14 +0600

    Test: This should pass without any type

---
+++ test-any.ts
@@ -0,0 +1,8 @@
+// This file is for testing - should pass commit now
+export function goodFunction(param: string) {
+  return param;
+}
+
+export function anotherGoodFunction(param: number) {
+  return param * 2;
+}
